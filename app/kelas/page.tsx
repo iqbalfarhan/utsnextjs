af963b8155e9fbb25719854f7516112f5342e2c2
iqbalfarhan <iqbalfarhan1996@gmail.com>
2024-12-23 02:41:03 +0800

    install prisma

--- app/kelas/page.tsx
+++ app/kelas/page.tsx
@@ -1,7 +1,46 @@
+import { Button } from '@/components/ui/button';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { PlusCircle } from 'lucide-react';
+
 const KelasPage = () => {
   return (
-    <div>
+    <div className='space-y-4'>
       <h2 className='text-xl font-bold'>Data kelas</h2>
+      <p>
+        Edit halaman ini untuk mengerjakan crud data kelas. Kriteria yang
+        diterima untuk mendapatkan nilai dari halaman ini adalah: terdapat table
+        data kelas dengan kolom id dan nama kelas. serta terdapat action untuk
+        create, edit dan delete data kelas. isi minimal 3 data kelas.
+      </p>
+      <Button>
+        <PlusCircle />
+        <span>Tambah data kelas</span>
+      </Button>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Id Kelas</TableHead>
+            <TableHead>Nama Kelas</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>1</TableCell>
+            <TableCell>ASE 02</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>2</TableCell>
+            <TableCell>CON 01</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 };
--- app/mahasiswa/page.tsx
+++ app/mahasiswa/page.tsx
@@ -1,7 +1,14 @@
 const MahasiswaPage = () => {
   return (
-    <div>
+    <div className='space-y-4'>
       <h2 className='text-xl font-bold'>Data mahasiswa</h2>
+      <p>
+        Edit halaman ini untuk mengerjakan crud data mahasiswa. Kriteria yang
+        diterima untuk mendapatkan nilai dari halaman ini adalah: terdapat table
+        data mahasiswa dengan kolom id, nim, nama mahasiswa dan nama kelas.
+        serta terdapat action untuk create, edit dan delete data mahasiswa. isi
+        minimal 3 data mahasiswa.
+      </p>
     </div>
   );
 };
