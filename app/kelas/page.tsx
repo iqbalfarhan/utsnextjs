9cad0b9e4540410488c1c6f6ecad3805e552f3c9
iqbalfarhan <iqbalfarhan1996@gmail.com>
2024-12-23 03:38:17 +0800

    perbarui halaman kelas

--- app/kelas/page.tsx
+++ app/kelas/page.tsx
@@ -1,14 +1,3 @@
-import { Button } from '@/components/ui/button';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table';
-import { PlusCircle } from 'lucide-react';
-
 const KelasPage = () => {
   return (
     <div className='space-y-4'>
@@ -19,28 +8,6 @@ const KelasPage = () => {
         data kelas dengan kolom id dan nama kelas. serta terdapat action untuk
         create, edit dan delete data kelas. isi minimal 3 data kelas.
       </p>
-      <Button>
-        <PlusCircle />
-        <span>Tambah data kelas</span>
-      </Button>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Id Kelas</TableHead>
-            <TableHead>Nama Kelas</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell>1</TableCell>
-            <TableCell>ASE 02</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>2</TableCell>
-            <TableCell>CON 01</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
     </div>
   );
 };
