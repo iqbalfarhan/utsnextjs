079a47604729739d0767cebb9aa4bf9727525aa2
Ryuzo911 <mhmmdrizalllll9@gmail.com>
2024-12-31 02:21:24 +0800

    Menambahkan tabel ke page kelas dan membuat directory untuk route api

--- app/kelas/page.tsx
+++ app/kelas/page.tsx
@@ -1,14 +1,75 @@
-const KelasPage = () => {
+
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { PrismaClient } from "@prisma/client";
+import { Edit, Trash } from "lucide-react";
+
+const prisma = new PrismaClient;
+
+const KelasPage = async () => {
+  const data = await prisma.kelas.findMany();
   return (
-    <div className='space-y-4'>
-      <h2 className='text-xl font-bold'>Data kelas</h2>
-      <p>
-        Edit halaman ini untuk mengerjakan crud data kelas. Kriteria yang
-        diterima untuk mendapatkan nilai dari halaman ini adalah: terdapat table
-        data kelas dengan kolom id dan nama kelas. serta terdapat action untuk
-        create, edit dan delete data kelas. isi minimal 3 data kelas.
-      </p>
-    </div>
+  <div>
+    
+    <Dialog>
+    <DialogTrigger>
+      <Button>Add kelas</Button>
+    </DialogTrigger>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Silahkan tambahkan data</DialogTitle>
+      </DialogHeader>
+      <div className="grid-gap-4 py-4">
+        <div className="grid grid-cols-4 grid-gap-4 items-center">
+          <Label className="text-left">Nama Kelas</Label>
+            <Input id="name" className="col-span-3" />
+        </div>
+      </div>
+      <DialogFooter>
+        <Button type="submit">Save</Button>
+      </DialogFooter>
+    </DialogContent>
+    </Dialog>
+
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>ID</TableHead>
+          <TableHead>Nama Kelas</TableHead>
+          <TableHead>Action </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data.map((Kelas) => (
+          <TableRow key={Kelas.id}>
+            <TableCell>{Kelas.id}</TableCell>
+            <TableCell>{Kelas.nama}</TableCell>
+            <TableCell>
+              <Dialog>
+                <DialogTrigger><Button variant={"outline"} >
+                <Edit/>
+              </Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Silahkan tambahkan data</DialogTitle>
+                </DialogHeader>
+              </DialogContent>
+              </Dialog>
+              <Button variant={"outline"}>
+                <Trash/>
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))
+       }
+      </TableBody>
+    </Table>
+  </div>
   );
 };
 
